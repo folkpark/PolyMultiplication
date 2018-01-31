c58acc696621ac1a65c299b8cedb7fe672b6bbd5
folkpark <parkerfolkman@gmail.com>
2018-01-30 17:21:39 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+Author: Parker Folkman
+Date: 1/26/2018
+Overview: This program implements the divide and 
+        conquer version of polynomial multiplication. 
+'''
+
+
+import math
+from timeit import default_timer as timer
+
+
+def nSquaredVersion(polyA, polyB):
+    polyC = []
+
+    return polyC
+
+
+def dAndCVersion(polyA, polyB):
+    polyC = []
+
+    return polyC
+
+nSquared_Outfile = open('nSquared.txt', 'w')
+nSquared_Outfile.write("Hello Jack")
+
+dAndC_Outfile = open('DC.txt', 'w')
+dAndC_Outfile.write("Hello Parker")
+
