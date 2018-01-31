1b0097cedb656edb44f4b16b75e57fefb6ed912f
folkpark <parkerfolkman@gmail.com>
2018-01-30 17:51:17 -0700

    Added split function

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ Overview: This program implements the divide and
         conquer version of polynomial multiplication. 
 '''
 
-
 import math
 from random import randint
 from timeit import default_timer as timer
@@ -17,24 +16,29 @@ def nSquaredVersion(polyA, polyB):
     return polyC
 
 
-def dAndCVersion(polyA, polyB):
-    polyC = []
+def dAndCVersion(polyA, polyB, n):
+    polyC = [] #need to remove this because of recursion
 
     return polyC
 
-nSquared_Outfile = open('nSquared.txt', 'w')
-nSquared_Outfile.write("Hello Jack")
+#function to split a list in half
+def split(A):
+    mid = len(A)/2
+    left = A[:mid]
+    right = A[mid:]
+    splitLists = [left,right]
+    return splitLists
 
+#Create output files
+nSquared_Outfile = open('nSquared.txt', 'w')
 dAndC_Outfile = open('DC.txt', 'w')
-dAndC_Outfile.write("Hello Parker")
 
 #Get some polynomials with random coefficients
+degree = 3  #polynomials are of same degree
 polyA = []
-a_degree = 3
-for i in range(a_degree):
+for i in range(degree):
     polyA.append(randint(0,9))
 
 polyB = []
-b_degree = 3
-for j in range(b_degree):
+for j in range(degree):
     polyB.append(randint(0, 9))
